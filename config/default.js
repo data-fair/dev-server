f58950f229b8bdfe37fb224373f5aea8e93fd6fc
Alban Mouton <alban.mouton@gmail.com>
2021-10-11 17:12:40 +0200

    fix: remove deprecated prefix in koumoul data-fair

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 module.exports = {
   port: 5888,
   dataFair: {
-    url: 'https://koumoul.com/s/data-fair',
+    url: 'https://koumoul.com/data-fair',
     owner: {
       type: 'organization',
       id: '5a5dc47163ebd4a6f438589b'
