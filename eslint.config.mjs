824096a819c4b39d7348e13aa021289faf9be928
Alban Mouton <alban.mouton@gmail.com>
2025-07-01 16:57:27 +0200

    chore: ignore type declaration files in linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import neostandard from 'neostandard'
 import dfLibRecommended from '@data-fair/lib-utils/eslint/recommended.js'
 
 export default [
-  { ignores: ['ui/*', '**/.type/', 'node_modules/*', 'test-apps/*', 'src/*.js'] },
+  { ignores: ['ui/*', '**/.type/', 'node_modules/*', 'test-apps/*', 'src/*.js', '**/*.d.ts'] },
   ...dfLibRecommended,
   ...neostandard({ ts: true })
 ]
